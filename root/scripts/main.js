242360fe2cde3290482ee9c6a69de29b9c3842a2
Yazmin <yazminrios116@gmail.com>
2023-06-15 20:53:15 -0500

    on it

--- root/scripts/main.js
+++ root/scripts/main.js
@@ -1,1 +1,22 @@
-alert("Hello world")
+const url = 'https://pokeapi.co/api/v2/pokemon';
+
+// // const character = fetch(`${url}/character`)
+// // .then(res => res.json())
+// // .then(data => {
+// //     data.results.forEach(character => {
+// //         console.log(character.name)
+        
+// //     });
+// // })
+
+const fetchApi = async (url) => {
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data);
+    } catch (error) {
+        
+    }
+}
+
+fetchApi(url)
